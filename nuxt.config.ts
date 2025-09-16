bcf8d3db6a7340e7da081ab9a6307ece0525f2c2
sobol4156 <sobol4156@gmail.com>
2025-09-16 21:38:12 +0300

    feat: added fonts, styles, icons; chore: deleted svgo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@pinia/nuxt"],
+  css: ["~/shared/assets/css/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/shared/assets/css/variables.scss" as *;',
+        },
+      },
+    },
+    assetsInclude: ["**/*.woff2", "**/*.woff", "**/*.ttf"],
+  },
 });
